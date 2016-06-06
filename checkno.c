307e8f54c1bd9077b37206c7900f0be8ed5903c9
SuthiyaKalidoss <ksuthiya@gmail.com>
2016-06-06 14:13:23 +0530

    Create checkno.c

---
+++ checkno.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void) {
+	// your code goes here
+	int n;
+	scanf("%d",&n);
+	if(n<0)
+	printf("negative no");
+	else if(n>0)
+	printf("positive no");
+	else
+	printf("zero");
+	return 0;
+}
